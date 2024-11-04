97aa1d05c32ddf95a215decbca82a21dfd0674bf
Teo Risso <68182473+teorisso@users.noreply.github.com>
2024-11-04 15:42:50 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String url = "jdbc:mysql://localhost:3306/sistemaacademico";
+        String usuario = "root";
+        String password = "";
+
+        String sql = "CREATE TABLE IF NOT EXISTS Usuarios (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "nombre VARCHAR(50) NOT NULL, " +
+                "email VARCHAR(50), " +
+                "edad INT" +
+                ");";
+
+        String nombre = "Juan Perez";
+        String email = "juan.perez@example.com";
+        int edad = 30;
+
+        String sql1 = "INSERT INTO Usuarios (nombre, email, edad) VALUES (?, ?, ?)";
+
+        try {
+            Connection conexion = DriverManager.getConnection(url, usuario, password);
+
+            Statement statement = conexion.createStatement();
+            statement.executeUpdate(sql);
+
+            PreparedStatement preparedStatement = conexion.prepareStatement(sql1);
+            preparedStatement.setString(1, nombre);
+            preparedStatement.setString(2, email);
+            preparedStatement.setInt(3, edad);
+
+            int filasAfectadas = preparedStatement.executeUpdate();
+            System.out.println("Filas insertadas: " + filasAfectadas);
+
+            System.out.println("Conexión exitosa");
+
+            preparedStatement.close();
+            statement.close();
+            conexion.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión");
+            e.printStackTrace();
         }
+
     }
 }
